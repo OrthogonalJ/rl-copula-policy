33dbbfe7e059a2d6f77faeb12c2332be28e6cd3c
Jordan Finch <jordan.finch@student.uts.edu.au>
2019-12-17 23:33:03 +1100

    Change gaussian_copula.py constructor and CDF method to avoid computing cholesky decomposition (fails for non semi-positive definite network outputs)

--- rl_copula_policy/tf_distributions/gaussian_copula.py
+++ rl_copula_policy/tf_distributions/gaussian_copula.py
@@ -10,20 +10,25 @@ class GaussianCopula(tfd.MultivariateNormalTriL):
             loc(tensor)
             covariance_matrix(tensor)
         """
-        scale_mat = tf.matmul(scale_tril, scale_tril, transpose_b=True)
-        print_op = tf.compat.v1.print('scale_mat:', scale_mat)
-        with tf.control_dependencies([print_op]):
-            diagonal = tf.ones(shape=shape_list(scale_mat)[:-1])
-        scale_tril = tf.linalg.set_diag(tf.linalg.cholesky(scale_mat), diagonal)
+        # scale_mat = tf.matmul(scale_tril, scale_tril, transpose_b=True)
+        # print_op = tf.compat.v1.print('scale_mat:', scale_mat)
+        # with tf.control_dependencies([print_op]):
+        #     diagonal = tf.ones(shape=shape_list(scale_mat)[:-1])
+        # scale_tril = tf.linalg.set_diag(tf.linalg.cholesky(scale_mat), diagonal)
+        self._loc = loc
+        self._scale_tril = scale_tril
         super(GaussianCopula, self).__init__(loc , scale_tril, validate_args=True)
     
     def cdf(self, value):
-        # print('#' * 80)
-        # print('VALUE:', value)
-        # print('#' * 80)
-        # num_components = value.get_shape().as_list()[-1]
+        # print('self._variances():', self._variances())
+        # print('self._loc:', self._loc)
+        # normal_dist = tfd.Normal(loc=self._loc, scale=self._variances())
+        # cdf_vals = normal_dist.cdf(value)
+        
+        std_normal_value = (value - self._loc) / self._stddev()
+        
         normal_cdf = tfb.NormalCDF()
-        value_parts = tf.unstack(value, axis=-1)
+        value_parts = tf.unstack(std_normal_value, axis=-1)
         cdf_vals = [normal_cdf.forward(value_part) for value_part in value_parts]
         cdf_vals = tf.concat(cdf_vals, axis=-1)
         
@@ -33,6 +38,14 @@ class GaussianCopula(tfd.MultivariateNormalTriL):
         cdf_vals = tf.reshape(cdf_vals, non_data_shape + [num_components])
 
         return cdf_vals
+
+    def _variances(self):
+        # Reverse the cholesky decomposition
+        cov_mat = tf.matmul(self._scale_tril, self._scale_tril, transpose_b=True)
+        return tf.linalg.diag_part(cov_mat)
+    
+    def _stddev(self):
+        return tf.math.sqrt(self._variances())
 
 # class GaussianCopula(tfd.TransformedDistribution):
 #     def __init__(self, loc, scale_tril):
